015a882ab6450898bad037244ac4ee7b8685e220
Bilboking <brkbendreality@gmail.com>
2020-12-23 09:05:55 -0600

    v-for

--- app.js
+++ app.js
@@ -2,27 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0,
+            books: [
+                {title: 'Name of the Wind', author:'Patrick Tothfus' },
+                {title: 'The Way of Kings', author:'Brandon Sanderson' },
+                {title: 'The Final Empire', author:'Brandon Sanderson' }
+            ]
+            
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data){
-            console.log(e, e.type)
-            if(data){
-            console.log(data)
-            }
-        },
-        handleMousemove(e){
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
+       
     }
     
 })
